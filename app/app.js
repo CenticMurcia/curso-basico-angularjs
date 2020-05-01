cd0c1a78334bd32769daf515af3bb4f690353f7d
Jon Lucas <jonatanlucasmolina@gmail.com>
2020-05-01 21:34:00 +0200

    second controller

--- app/app.js
+++ app/app.js
@@ -4,5 +4,6 @@
  * En el array de dependencias del módulo principal añadiremos el submódulo que hemos creado para nuestro controlador HelloController.
  */
 angular.module("myApp", [
-  'myApp.firstSubModule'
+  'myApp.firstSubModule',
+  'myApp.secondSubModule'
 ]);
---
+++ app/components/secondComponent/second-component.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Crearemos un submódulo a partir del módulo principal llamado firstSubModule.
+ */
+angular.module('myApp.secondSubModule',[])
+
+/**
+ * Al controlador se le debe de añadir la dependencia del '$scope' principal y así podremos utilizar el componente en el 
+ * index.html
+ */
+.controller("MathController", ['$scope',function($scope){
+    $scope.double = function(value) { return value * 2; };
+}]);
